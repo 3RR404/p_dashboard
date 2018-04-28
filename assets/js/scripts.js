190f2143c825e6f5875ef8c95b61367be46b4109
oknaj <oknajlehcim@gmail.com>
2018-04-28 15:21:38 +0200

    ...

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,33 +4,27 @@
 
 	jQuery( document ).ready( function( $ ){
 
+		  var magicLine = $('div.magic-line');
 
-	    var $el, leftPos, newWidth,
-	        $mainNav = $(".navbar-white .nav-items");
+		  magicLine.width($(".current").width())
+			        .css("left", $("a.active").position().left)
+			        .data("origLeft", magicLine.position().left)
+			        .data("origWidth", magicLine.width());
 
-	    $mainNav.append("<li class='magic-line'></li>");
-	    var $magicLine = $(".magic-line");
-
-	    $magicLine
-	        .width($("nav-item.current").width())
-	        .css("left", $("nav-item.current a").position().left)
-	        .data("origLeft", $magicLine.position().left)
-	        .data("origWidth", $magicLine.width());
-
-	    $(".navbar-white li a").hover(function() {
-	        $el = $(this);
-	        leftPos = $el.position().left;
-	        newWidth = $el.parent().width();
-	        $magicLine.stop().animate({
-	            left: leftPos,
-	            width: newWidth
-	        });
-	    }, function() {
-	        $magicLine.stop().animate({
-	            left: $magicLine.data("origLeft"),
-	            width: $magicLine.data("origWidth")
-	        });
-	    });
-	});
+		  $(".navbar-white li a").hover(function() {
+			        var el = $(this);
+			        var leftPos = el.position().left;
+			        var newWidth = el.parent().width();
+			        magicLine.stop().animate({
+			            left: leftPos,
+			            width: newWidth
+			        });
+			    }, function() {
+			        magicLine.stop().animate({
+			            left: magicLine.data("origLeft"),
+			            width: magicLine.data("origWidth")
+			        });
+			    });
+	});//if document is ready (is loaded)
 
 })(window.jQuery);
