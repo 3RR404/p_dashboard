826cc22257a8c50a49be32aa2cf53e6516cda1d5
oknaj <oknajlehcim@gmail.com>
2018-05-10 21:14:55 +0200

    restore part of script
    bad place for that

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -251,19 +251,18 @@
 				$('a[href="#edit"]').show();
 				e.preventDefault();
 			});
-
-			if (typeof(Storage) !== "undefined") {
-
-		    var headerTitle = $("#user-name"),
-				brandTitle = $('#brand-title');
-				headerTitle.html( localStorage.getItem("username") );
-				brandTitle.html( localStorage.getItem("username") );
-			} else {
-			    document.getElementById("user-name").innerHTML = "HOANG NGUYEN";
-				document.getElementById("brand-title").innerHTML = "HOANG NGUYEN";
-			}
 		});
 
+		if (typeof(Storage) !== "undefined") {
+
+		var headerTitle = $("#user-name"),
+			brandTitle = $('#brand-title');
+			headerTitle.html( localStorage.getItem("username") );
+			brandTitle.html( localStorage.getItem("username") );
+		} else {
+			document.getElementById("user-name").innerHTML = "HOANG NGUYEN";
+			document.getElementById("brand-title").innerHTML = "HOANG NGUYEN";
+		}
 
 	});//if document is ready (is loaded
 
